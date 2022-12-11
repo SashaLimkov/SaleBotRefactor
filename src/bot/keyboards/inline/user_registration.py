81c3d28b2412873e973a744ac09a6aa7a450b144
SashaLimkov <ghalimov.mansur2@mail.ru>
2022-12-11 22:45:04 +0300

    smt

---
+++ src/bot/keyboards/inline/user_registration.py
@@ -0,0 +1,20 @@
+from bot.utils.keyboard_utils.base_keyboard_utils import get_base_keyboard
+
+__all__ = [
+    "get_start_registration_keyboard",
+]
+
+
+async def get_start_registration_keyboard():
+    await get_base_keyboard(
+        buttons=[
+            {
+                "text": "Начать регистрацию",
+                "callback_data": "start_reg"
+            },
+        ]
+    )
+
+
+async def get_user_registration_menu():
+    pass
