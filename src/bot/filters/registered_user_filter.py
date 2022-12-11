71e3d096e7870f7b7975c9c7b50b92f146341a37
SashaLimkov <ghalimov.mansur2@mail.ru>
2022-12-11 20:12:02 +0300

    add is_registered filter

---
+++ src/bot/filters/registered_user_filter.py
@@ -0,0 +1,14 @@
+from aiogram import types
+from aiogram.dispatcher.filters import BoundFilter
+from bot.data import list_and_tuple_data as ld
+
+
+class IsRegistered(BoundFilter):
+    async def check(self, message: types.Message | types.CallbackQuery) -> bool:
+        match type(message):
+            case types.Message:
+                return message.chat.id in ld.ADMINS
+            case types.CallbackQuery:
+                return message.message.chat.id in ld.ADMINS
+            case _:
+                return False
