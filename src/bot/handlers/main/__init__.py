a5c6f9a1e9c244994934418ee24903d5e19e7757
SashaLimkov <ghalimov.mansur2@mail.ru>
2022-12-18 01:06:22 +0300

    registration by deep_link fix

--- src/bot/handlers/main/__init__.py
+++ src/bot/handlers/main/__init__.py
@@ -2,12 +2,14 @@ from aiogram import Dispatcher
 from aiogram.dispatcher import filters
 from bot.filters import IsAdmin, IsRegistered
 
-from bot.handlers.main import registration_module, main_menu_actions, user_settings, channels, subscriptions
+from bot.handlers.main import registration_module, main_menu_actions, user_settings, channels, subscriptions, \
+    no_user_rights_module
 from bot.handlers.main import commands
 
 
 def setup(dp: Dispatcher):
     registration_module.setup(dp)
+    no_user_rights_module.setup(dp)
     main_menu_actions.setup(dp)
     user_settings.setup(dp)
     channels.setup(dp)
